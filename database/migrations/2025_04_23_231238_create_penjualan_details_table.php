4cc8c7f639d21aeead8c612ca597c58398ed9ac8
Vikar Maulana Arrisyad <vikar.maulana.arrisyad@gmail.com>
2025-04-24 06:15:53 +0700

    add file migration

---
+++ database/migrations/2025_04_23_231238_create_penjualan_details_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('penjualan_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('penjualan_id');
+            $table->unsignedBigInteger('produk_id');
+            $table->integer('harga')->default(0);
+            $table->integer('jumlah')->default(0);
+            $table->integer('total_harga')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('penjualan_details');
+    }
+};
